ea021042caa32dcab7c579a9ef5f163ab3ad770f
macbookpro <macbookpro@10.0.0.147>
2016-09-18 15:20:11 -0700

    Completed database method

--- src/WordDatabase.java
+++ src/WordDatabase.java
@@ -1,8 +1,35 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class WordDatabase
 {
-	public void addToDatabase(String word, String path, int position)
+	private static TreeMap<String, TreeMap<String, TreeSet<Integer>>> database;
+	public void addToDatabase(String word, String stringPath, int position)
 	{
-		
+		if(!database.containsKey(word))
+		{
+			TreeMap<String, TreeSet<Integer>> file = new TreeMap<String, TreeSet<Integer>>();
+			TreeSet<Integer> index = new TreeSet<Integer>();
+			index.add(position);
+			file.put(stringPath, index);
+			database.put(word, file);
+		}
+		else
+		{
+			TreeMap<String, TreeSet<Integer>> file = database.get(word);
+			if(file.containsKey(stringPath))
+			{
+				TreeSet<Integer> index = file.get(stringPath);
+				index.add(position);
+				database.put(word, file);
+			}
+			else
+			{
+				TreeSet<Integer> index = new TreeSet<Integer>();
+				index.add(position);
+				file.put(stringPath, index);
+				database.put(word, file);
+			}
+		}
 	}
 }
